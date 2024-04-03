08ca526413ee3dc7af21bfc0178b1b310ffe50dc
Seung-Jong Oh <tmdwhd319@gmail.com>
2024-04-04 08:41:14 +0900

    matrix 최대합 이중배열로 풀기

--- section_2/격자판_최대합.js
+++ section_2/격자판_최대합.js
@@ -1,32 +1,52 @@
 function solution(n = 0, ...rest) {
-  let answer = 0;
+  let answer = Number.MIN_SAFE_INTEGER;
 
-  // 1. 각 행의 합. 5개
-  const row = rest.map((val) => val.reduce((prev, cur) => prev + cur, 0));
-  const MAX_ROW = Math.max(...row);
+  // // 1. 각 행의 합. 5개
+  // const row = rest.map((val) => val.reduce((prev, cur) => prev + cur, 0));
+  // const MAX_ROW = Math.max(...row);
 
-  // 2. 각 열의 합. 5개
-  const col = rest.reduce((prev, cur) => {
-    let tempArr = [];
-    for (let i = 0; i < n; i++) {
-      let temp = prev[i] + cur[i];
-      tempArr.push(temp);
+  // // 2. 각 열의 합. 5개
+  // const col = rest.reduce((prev, cur) => {
+  //   let tempArr = [];
+  //   for (let i = 0; i < n; i++) {
+  //     let temp = prev[i] + cur[i];
+  //     tempArr.push(temp);
+  //   }
+
+  //   return tempArr;
+  // });
+  // const MAX_COL = Math.max(...col);
+
+  // // 3. 대각선의 합. 2개
+  // const diagonalMain = rest.reduce((prev, cur, idx) => prev + cur[idx], 0);
+
+  // const diagonalSub = rest.reduce(
+  //   (prev, cur, idx) => prev + cur[n - 1 - idx],
+  //   0
+  // );
+
+  // // 최종 MAX 구하기.
+  // answer = Math.max(MAX_ROW, MAX_COL, diagonalMain, diagonalSub);
+
+  for (let i = 0; i < n; i++) {
+    let row = 0;
+    let col = 0;
+    for (let j = 0; j < n; j++) {
+      row += rest[i][j];
+      col += rest[j][i];
     }
+    answer = Math.max(answer, row, col);
+  }
 
-    return tempArr;
-  });
-  const MAX_COL = Math.max(...col);
+  let mainDia = 0;
+  let subDia = 0;
 
-  // 3. 대각선의 합. 2개
-  const diagonalMain = rest.reduce((prev, cur, idx) => prev + cur[idx], 0);
+  for (let i = 0; i < n; i++) {
+    mainDia += rest[i][i];
+    subDia += rest[i][n - 1 - i];
+  }
 
-  const diagonalSub = rest.reduce(
-    (prev, cur, idx) => prev + cur[n - 1 - idx],
-    0
-  );
-
-  // 최종 MAX 구하기.
-  answer = Math.max(MAX_ROW, MAX_COL, diagonalMain, diagonalSub);
+  answer = Math.max(answer, mainDia, subDia);
 
   return answer;
 }
